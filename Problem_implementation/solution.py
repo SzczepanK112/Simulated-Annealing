ae76b3a4c44ad26b0feb4f3e50fc55925d7eb204
SzczepanKAGH <skonior@student.agh.edu.pl>
2024-11-25 15:26:53 +0100

    machine class and initial path creator

---
+++ Problem_implementation/solution.py
@@ -0,0 +1,75 @@
+import random
+import struktury_danych
+import wczytanie_mapy
+from typing import List, Union
+
+
+class Machine:
+    def __init__(self, speed=1):
+        self.speed = speed
+        self.route = []
+
+    def generate_initial_route(self, road_layout, Tmax, number_of_stages):
+        current_location = road_layout.baza
+
+        for stage_no in range(number_of_stages):
+            time_cost = 0
+            stage_route = []
+
+            # For each stage of snowfall calculate random route that is allowed by the time constraints
+            while True:
+                neighbors = current_location.sasiedzi
+                next_location = random.choice(neighbors)  # Choose random neighbor as next location
+                street = road_layout.get_edge(current_location, next_location)
+
+                time_cost += street.oblicz_dlugosc() / self.speed
+
+                if time_cost >= Tmax:
+                    break
+
+                stage_route.append({"p1": current_location, "p2": next_location, "street": street})
+                current_location = next_location
+
+            self.route.append(stage_route)
+
+
+class RoadClearingProblem:
+    def __init__(self,
+                 snowfall_forecast: List[int],
+                 road_layout: struktury_danych.Graf,
+                 machines: List[Machine],
+                 Tmax: Union[int, float]):
+
+        """
+        snowfall_forecast - vector containing level of snow that will cover the streets after each time period
+        road_layout - graph with road representation for a given city
+        machines - list of machines designated for snow removal
+        Tmax - max time that machine can spend clearing the streets before next snowfall | time between snowfalls
+        danger - goal function to be minimized (our temperature as in simulated annealing)
+        """
+
+        self.snowfall_forecast = snowfall_forecast
+        self.road_layout = road_layout
+        self.solution = road_layout
+        self.machines = machines
+        self.danger = float("inf")
+        self.Tmax = Tmax
+
+    def calculate_danger(self):
+        temp_danger = 0
+        for street in self.solution.krawedzie:
+            temp_danger += street.get_danger_level()
+
+        self.danger = temp_danger
+
+    def get_initial_path(self):
+        for machine in self.machines:
+            machine.generate_initial_route(self.road_layout, self.Tmax, len(self.snowfall_forecast))
+
+    def find_new_path(self):
+        pass
+
+    def mainloop(self):
+        for snowfall in self.snowfall_forecast:
+            for street in self.solution.krawedzie:
+                street.snow_level += snowfall
--- Problem_implementation/struktury_danych.py
+++ Problem_implementation/struktury_danych.py
@@ -2,7 +2,8 @@ import math
 import matplotlib.pyplot as plt
 import networkx as nx
 
-class Wierzcholek: # Obrazuje poczatek/koniec ulicy lub skrzyzowanie ulic
+
+class Wierzcholek:  # Obrazuje poczatek/koniec ulicy lub skrzyzowanie ulic
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -13,29 +14,33 @@ class Wierzcholek: # Obrazuje poczatek/koniec ulicy lub skrzyzowanie ulic
 
     def __repr__(self):
         return f"({self.x}, {self.y})"
-    
-    def __eq__(self, other): # Pozwala porownac identycznosc dwoch wierzcholkow
+
+    def __eq__(self, other):  # Pozwala porownac identycznosc dwoch wierzcholkow
         if isinstance(other, Wierzcholek):
             return (self.x, self.y) == (other.x, other.y)
         return False
-    
 
-class Krawedz: # Obrazuje ulice polaczona przez dwa wierzcholki
+
+class Krawedz:  # Obrazuje ulice polaczona przez dwa wierzcholki
     def __init__(self, start, koniec, priorytet=0, pasy=1):
         self.start = start
         self.koniec = koniec
-        self.priorytet = priorytet # priorytet w zakresie 0-100
+        self.priorytet = priorytet  # priorytet w zakresie 0-100
         self.pasy = pasy  # ilosc pasow
-        self.dlugosc = self.oblicz_dlugosc() 
-        
+        self.dlugosc = self.oblicz_dlugosc()
+        self.snow_level = 0
+
     def oblicz_dlugosc(self):
         return math.sqrt((self.start.x - self.koniec.x) ** 2 + (self.start.y - self.koniec.y) ** 2)
 
     def __repr__(self):
         return f"{self.start} -> {self.koniec}"
-    
 
-class Graf: # Obrazuje pelny rozklad ulic/skrzyzowan
+    def get_danger_level(self):
+        return self.snow_level * self.priorytet * self.pasy
+
+
+class Graf:  # Obrazuje pelny rozklad ulic/skrzyzowan
     def __init__(self):
         self.wierzcholki = []
         self.krawedzie = []
@@ -54,6 +59,25 @@ class Graf: # Obrazuje pelny rozklad ulic/skrzyzowan
         if w not in self.wierzcholki:
             self.wierzcholki.append(w)
         return w
+
+    def get_edge(self, point1, point2):
+        """
+        Znajduje krawędź pomiędzy dwoma wierzchołkami (point1, point2).
+        Zwraca krawędź, jeśli istnieje, w przeciwnym przypadku zwraca None.
+
+        Args:
+        - point1: krotka (x1, y1) reprezentująca pierwszy wierzchołek
+        - point2: krotka (x2, y2) reprezentująca drugi wierzchołek
+
+        Returns:
+        - krawedz: Krawędź między wierzchołkami lub None, jeśli krawędź nie istnieje
+        """
+        for krawedz in self.krawedzie:
+            # Sprawdzamy, czy punkt1 jest początkiem krawędzi, a punkt2 końcem
+            if (krawedz.start.x, krawedz.start.y) == point1 and (krawedz.koniec.x, krawedz.koniec.y) == point2:
+                return krawedz
+
+        return None  # Jeśli nie znaleziono krawędzi
 
     def dodaj_krawedz(self, punkt1, punkt2, priorytet, pasy):
         # Dodaje krawędź do grafu między punktami (x1, y1) a (x2, y2), uwzględniając kierunek.
@@ -74,7 +98,7 @@ class Graf: # Obrazuje pelny rozklad ulic/skrzyzowan
         for krawedz in self.krawedzie:
             result += f"  {krawedz}\n"
         return result
-    
+
     def rysuj(self):
         G = nx.Graph()
 
@@ -90,20 +114,22 @@ class Graf: # Obrazuje pelny rozklad ulic/skrzyzowan
             edge_labels[((k.start.x, k.start.y), (k.koniec.x, k.koniec.y))] = f"Pr: {k.priorytet}, Pasy: {k.pasy}"
 
         # Określamy pozycje wierzchołków
-        pos = { (w.x, w.y): (w.x, w.y) for w in self.wierzcholki }
+        pos = {(w.x, w.y): (w.x, w.y) for w in self.wierzcholki}
 
         # Zwiększenie rozmiar obrazu
-        plt.figure(figsize=(10, 10)) 
+        plt.figure(figsize=(10, 10))
 
         # Rysowanie grafu
-        nx.draw(G, pos, with_labels=True, node_size=600, node_color='skyblue', font_size=10, font_weight='bold', edge_color='gray', width=4)
+        nx.draw(G, pos, with_labels=True, node_size=600, node_color='skyblue', font_size=10, font_weight='bold',
+                edge_color='gray', width=4)
 
         # Rysowanie etykiet krawędzi
         nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=6)
 
         # Rysowanie punktu początkowego (baza) na czerwono, ale z pustym środkiem
         if self.baza:
-            plt.scatter(self.baza.x, self.baza.y, color='red', s=750, label='Baza', edgecolors='red', facecolors='none', zorder=5, linewidth=3)
+            plt.scatter(self.baza.x, self.baza.y, color='red', s=750, label='Baza', edgecolors='red', facecolors='none',
+                        zorder=5, linewidth=3)
 
         # Wyświetlanie grafu z legendą
         plt.legend()
